0e62db727ea9b45e06a0aac6072a8cfdf8d09b18
Paulo César Dianin Junior <lolapa@uol.com.br>
2012-01-28 21:16:41 -0200

    #layout + home page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bdsite::Application.config.secret_token = 'eab722c2d58a77ffd1195e5301dabd98206c15709640ca36dc108bf9434528ec1f649a94cd403f7b947a829b348f0a31a01f5e6cd78a75ce8b8dc223ccbb5823'
