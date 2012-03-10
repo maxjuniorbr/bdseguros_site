48d57dec0631af37914887064311154e42a12089
Paulo César Dianin Junior <lolapa@uol.com.br>
2012-03-10 20:49:37 -0300

    #insurance quotes form and info

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,10 +5,22 @@ class QuotesController < ApplicationController
     @quote_types = [["Automóvel", 0], ["Residencial", 1], ["Moto", 2], ["Equipamentos portáteis", 3], ["Aluguel", 4], ["Vida", 5], ["Empresarial", 6], ["Condomínio", 7]]
     @br_states = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO", "RR", "RS", "SC", "SE", "SP", "TO"]
 
-    if params[:quote][:type_quote]
-      @type_quote = params[:quote][:type_quote]
-    end if params[:quote]
+    #Render by quote type
+    if params[:quote]
+      if params[:quote][:type_quote]
+        @quote_type_quote = params[:quote][:type_quote]
+        case @quote_type_quote
+        when "0"
+          @partial = "auto"
+        else
+          @partial = nil
+        end
+      end
+    end
 
-    render :layout => "application_new"
+    if params[:commit] == "Iniciar simulação"
+    end
+
+    render :layout => "application_new"    
   end
 end
